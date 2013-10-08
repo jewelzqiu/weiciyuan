dbaf190ea39a575c0c4d0be4937cdbd0fc7d0fe5
jewelzqiu <jewelz.q.915@gmail.com>
2013-10-08 15:01:59 +0800

    Add swipe to close gesture to UserTimeLineActivity.

--- src/org/qii/weiciyuan/ui/userinfo/UserTimeLineActivity.java
+++ src/org/qii/weiciyuan/ui/userinfo/UserTimeLineActivity.java
@@ -2,11 +2,23 @@ package org.qii.weiciyuan.ui.userinfo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.GestureDetector;
 import android.view.MenuItem;
+import android.view.View;
+
+import org.qii.weiciyuan.R;
 import org.qii.weiciyuan.bean.UserBean;
+import org.qii.weiciyuan.support.lib.AppFragmentPagerAdapter;
+import org.qii.weiciyuan.support.lib.MyViewPager;
+import org.qii.weiciyuan.support.lib.SwipeRightToCloseOnGestureListener;
 import org.qii.weiciyuan.ui.interfaces.AbstractAppActivity;
 import org.qii.weiciyuan.ui.interfaces.IUserInfo;
 import org.qii.weiciyuan.ui.main.MainTimeLineActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * User: qii
@@ -14,6 +26,9 @@ import org.qii.weiciyuan.ui.main.MainTimeLineActivity;
  */
 public class UserTimeLineActivity extends AbstractAppActivity implements IUserInfo {
     private UserBean bean;
+    private String token;
+    private MyViewPager mViewPager;
+    private GestureDetector gestureDetector;
 
 
     @Override
@@ -21,21 +36,26 @@ public class UserTimeLineActivity extends AbstractAppActivity implements IUserIn
         return bean;
     }
 
+    public String getToken() {
+        return token;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewpager_with_bg_layout);
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setDisplayShowTitleEnabled(true);
         getActionBar().setDisplayShowHomeEnabled(false);
-        String token = getIntent().getStringExtra("token");
+        token = getIntent().getStringExtra("token");
         bean = (UserBean) getIntent().getParcelableExtra("user");
         getActionBar().setTitle(bean.getScreen_name());
-        if (getSupportFragmentManager().findFragmentByTag(StatusesByIdTimeLineFragment.class.getName()) == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(android.R.id.content, new StatusesByIdTimeLineFragment(getUser(), token), StatusesByIdTimeLineFragment.class.getName())
-                    .commit();
-        }
-
+//        if (getSupportFragmentManager().findFragmentByTag(StatusesByIdTimeLineFragment.class.getName()) == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .replace(android.R.id.content, new StatusesByIdTimeLineFragment(getUser(), token), StatusesByIdTimeLineFragment.class.getName())
+//                    .commit();
+//        }
+        buildViewPager();
     }
 
     @Override
@@ -49,5 +69,47 @@ public class UserTimeLineActivity extends AbstractAppActivity implements IUserIn
                 return true;
         }
         return false;
+    }
+
+    private void buildViewPager() {
+        mViewPager = (MyViewPager) findViewById(R.id.viewpager);
+        TimeLinePagerAdapter adapter = new TimeLinePagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(adapter);
+        mViewPager.setOverScrollMode(View.OVER_SCROLL_NEVER);
+        gestureDetector = new GestureDetector(UserTimeLineActivity.this
+                , new SwipeRightToCloseOnGestureListener(UserTimeLineActivity.this, mViewPager));
+        mViewPager.setGestureDetector(this, gestureDetector);
+        getWindow().setBackgroundDrawable(getResources().getDrawable(R.color.transparent));
+    }
+
+    class TimeLinePagerAdapter extends AppFragmentPagerAdapter {
+
+        List<Fragment> list = new ArrayList<Fragment>();
+        List<String> tagList = new ArrayList<String>();
+
+        public TimeLinePagerAdapter(FragmentManager fm) {
+            super(fm);
+            Fragment fragment = fm.findFragmentByTag(StatusesByIdTimeLineFragment.class.getName());
+            if (fragment == null) {
+                fragment = new StatusesByIdTimeLineFragment(getUser(), getToken());
+            }
+            list.add(fragment);
+            tagList.add(StatusesByIdTimeLineFragment.class.getName());
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        protected String getTag(int position) {
+            return tagList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
     }
 }
